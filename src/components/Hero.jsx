649ba9897ac72eeceb4e9c0027bf14f896a418b5
mayank151c <mayank151choudhary@gmail.com>
2025-07-27 21:47:27 +0530

    Add button to download resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { styles } from "../styles";
+const link = 'https://drive.google.com/uc?export=download&id=1oYFzZAhP3xWhijv-WvULMd54QCjLZq07';
 
 const Hero = () => {
   return (
@@ -18,9 +19,21 @@ const Hero = () => {
           <p className={`${styles.heroSubText} lg:mt-10 md:mt-6  mt-4 text-white-100`}>
             Fullstack Software Developer,<br/>Cloud & AI Enthusiast
           </p>
-          <p className={`text-[#dfd9ff] font-medium lg:text-[24px] sm:text-[22px] xs:text-[18px] text-[16px] lg:leading-[50px] md:mt-16 md:mt-12 mt-10 text-blue-100`}>
+          <p className={`text-[#dfd9ff] font-medium lg:text-[24px] sm:text-[22px] xs:text-[18px] text-[16px] lg:leading-[50px] md:mt-8 mt-6 text-blue-100`}>
             Building backend systems with purpose, precision, and power.
           </p>
+
+          <button
+            className="flex items-center gap-1 sm:text-[1.1rem] text-[.9rem] border rounded px-3 py-1 my-2 text-[#ffffff] bg-[#915EFF]"
+            onClick={() => window.open(link, "_blank")}
+          >
+            Resume
+            <img
+              className="w-5 h-5"
+              src='https://img.icons8.com/?size=100&id=20FjgTazh8FG&format=png&color=ffffff' 
+              alt='download' 
+            />
+          </button>
         </div>
       </div>
     </section>
